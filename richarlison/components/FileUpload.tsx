a0339c602ab8e13b684da2ba5e6575e3ead66157
TR Kizaki <trkizaki.web@gmail.com>
2023-09-24 22:45:06 +0400

    set up AWS S3

--- richarlison/components/FileUpload.tsx
+++ richarlison/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { uploadToS3 } from '@/lib/s3';
 import { Inbox } from 'lucide-react';
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
@@ -7,10 +8,23 @@ const FileUpload = () => {
     const { getRootProps, getInputProps } = useDropzone({
         accept: {'application/pdf':[".pdf"] },
         maxFiles: 1,
-        onDrop: (acceptedFiles) => {
+        onDrop: async (acceptedFiles) => {
             console.log(acceptedFiles);
-        }
+            const file = acceptedFiles[0];
+            if(file.size > 10 * 1024 * 1024) {
+                // bigger than 10mb!
+                alert('please upload a smaller file');
+                return;
+            }
 
+            try {
+            const data = await uploadToS3(file);
+            console.log("data", data);
+            } catch (error) {
+            console.log(error);
+
+            }
+        },
     });
   return (
   <div className="p-2 bg-white rounded-xl">
