0a7d46012f56352aa439d6aef122e2aa0d5ae88e
TR Kizaki <trkizaki.web@gmail.com>
2023-09-25 23:06:52 +0400

    React Query Set Up

--- richarlison/app/layout.tsx
+++ richarlison/app/layout.tsx
@@ -1,24 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {ClerkProvider} from '@clerk/nextjs'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import {ClerkProvider} from '@clerk/nextjs';
+import Providers from '@/components/Providers';
+import { Toaster } from 'react-hot-toast';
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Recharlison',
-}
+  title: 'Richarlison',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
+      <Providers>
        <html lang="en">
          <body className={inter.className}>{children}</body>
+         <Toaster />
        </html>
+      
+      </Providers>
+      
     </ClerkProvider>
-  )
+  );
 }
